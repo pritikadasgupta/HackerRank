3fbd067fe874c3864f66c0cd4abc05319d3dc785
pritikadasgupta <pritika.dasgupta@gmail.com>
2018-08-22 19:07:20 -0400

    starting day 25 - 28

---
+++ Python3/30DaysOfCode/day25.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def prime():
+
+if __name__ == '__main__':
+    tip_percent = int(input())
+
+    tax_percent = int(input())
