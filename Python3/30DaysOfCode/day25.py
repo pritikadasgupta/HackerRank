da6ef7c9a4d514fa2468f8406e9298b3c2a68dc3
pritikadasgupta <pritika.dasgupta@gmail.com>
2018-08-22 20:58:34 -0400

    tried code from stack overflow to check every sixth number after initial primes and it worked

--- Python3/30DaysOfCode/day25.py
+++ Python3/30DaysOfCode/day25.py
@@ -7,11 +7,23 @@ import re
 import sys
 
 # Complete the solve function below.
+def prime2(num):
+	result = "Prime"
+	if num % 2 == 0 or num <2: #even and 1
+		result = "Not prime"  
+	else: #odd numbers
+		for i in range(3,num+1,2):
+			if num % i == 0:
+				if i > 1 and i !=num:
+					result = "Not prime"
+					break
+	return result
+
+
+
 def prime(num):
 	result = "Prime"
 	if num % 2 == 0 or num <2: #even and 1
-		result = "Not prime"
-	elif num % 5 ==0:
 		result = "Not prime"
 	else: #odd numbers
 		for i in range(1,num+1):
@@ -20,6 +32,23 @@ def prime(num):
 					result = "Not prime"
 					break
 	return result
+
+
+# https://stackoverflow.com/questions/15285534/isprime-function-for-python-language
+def is_prime(n):
+  if n == 2 or n == 3: return True
+  if n < 2 or n%2 == 0: return False
+  if n < 9: return True
+  if n%3 == 0: return False
+  r = int(n**0.5)
+  f = 5
+  while f <= r:
+    # print '\t',f
+    if n%f == 0: return False
+    if n%(f+2) == 0: return False
+    f +=6
+  return True  
+
 
 def primes_sieve(limit):
     a = [True] * limit                          # Initialize the primality list
@@ -45,6 +74,25 @@ def primes_sieve2(limit):
             not_prime[f] = True
         primes.append(i)
     return primes
+# if __name__ == '__main__':
+#     test_cases = int(input())
+
+#     for i in range(0,test_cases):
+# 	    num = int(input())
+# 	    result = primes_sieve2(num)
+# 	    if num in result:
+# 	    	print("Prime")
+# 	    else:
+# 	    	print("Not prime")
+# 	    print(result)
+# if __name__ == '__main__':
+#     test_cases = int(input())
+
+#     for i in range(0,test_cases):
+# 	    num = int(input())
+# 	    result = prime2(num)
+# 	    print(result)
+
 
 
 if __name__ == '__main__':
@@ -52,9 +100,8 @@ if __name__ == '__main__':
 
     for i in range(0,test_cases):
 	    num = int(input())
-	    result = primes_sieve2(num)
-	    if num in result:
+	    result = is_prime(num)
+	    if result:
 	    	print("Prime")
 	    else:
 	    	print("Not prime")
-	    print(result)
