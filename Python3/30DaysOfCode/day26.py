0459f31192702bc25a20bf9a79ec2fb4cfc07763
pritikadasgupta <pritika.dasgupta@gmail.com>
2018-08-22 21:24:08 -0400

    finished code - trial 1 for day 26

--- Python3/30DaysOfCode/day26.py
+++ Python3/30DaysOfCode/day26.py
@@ -8,16 +8,25 @@ import sys
 import datetime
 
 # Complete the solve function below.
-def fine(date):
-  
-
-
-
+def fine(dayR, monthR, yearR, dayD, monthD, yearD):
+  if yearR > yearD: #book returned after calendar year in which it was expected
+    result = 10000
+  elif yearR == yearD: #within same calendar year
+    if monthR > monthD: #but after expected month
+      result = 500*(monthR - monthD)
+    if monthR == monthD: #within same month
+      if dayR == dayD or dayR < dayD: #on or before due day
+        result = 0
+      else:
+        result = 15*(dayR - dayD) #after expected day
+  return str(result)
 
 if __name__ == '__main__':
-  date_entry = input('')
-  day, month, year = map(int, date_entry.split(' '))
-  date1 = datetime.date(year, month, day)
-  print(fine(date1))
+  date_returned = input('')
+  date_due = input('')
+  dayR, monthR, yearR = map(int, date_returned.split(' '))
+  dayD, monthD, yearD = map(int, date_due.split(' '))
+  # date1 = datetime.date(year, month, day)
+  print(fine(dayR, monthR, yearR, dayD, monthD, yearD))
 
 
