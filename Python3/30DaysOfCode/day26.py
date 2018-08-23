b5d786024809896ac67b4a0986e381545739a19b
pritikadasgupta <pritika.dasgupta@gmail.com>
2018-08-22 21:15:45 -0400

    skeleton code for day 26

---
+++ Python3/30DaysOfCode/day26.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import datetime
+
+# Complete the solve function below.
+def fine(date):
+  
+
+
+
+
+if __name__ == '__main__':
+  date_entry = input('')
+  day, month, year = map(int, date_entry.split(' '))
+  date1 = datetime.date(year, month, day)
+  print(fine(date1))
+
+
