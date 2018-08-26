ff44ee83a5dd9a0c69e8cd8573f51ae9389bceae
pritikadasgupta <pritika.dasgupta@gmail.com>
2018-08-26 06:12:35 -0400

    day 29, it runs but times out on some test cases

--- Python3/30DaysOfCode/day29.py
+++ Python3/30DaysOfCode/day29.py
@@ -7,23 +7,19 @@ import re
 import sys
 
 def bitwise(n,k):
-	s = list(range(1,n+1,1))
-	a=1
-	b=k
-	for i in range(0,len(s)):
-		
+	result = 0
+    for j in range(1, n):
+        for i in range(j + 1, n + 1):
+            pointer = j & i
+            if k > pointer > result:
+                result = pointer
+    return result
 
-
-	a
-	b
-	aANDb
 
 if __name__ == '__main__':
     t = int(input())
-
     for t_itr in range(t):
         nk = input().split()
-
         n = int(nk[0])
-
         k = int(nk[1])
+        print(bitwise(n,k))
