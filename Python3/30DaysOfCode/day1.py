fef66a99898ab64e31a0f67f781257ce52ba5ed1
pritikadasgupta <pritika.dasgupta@gmail.com>
2018-09-02 13:16:22 -0400

    fixed day1

--- Python3/30DaysOfCode/day1.py
+++ Python3/30DaysOfCode/day1.py
@@ -1,11 +1,14 @@
 #!/bin/python3
 
+i = 4
+d = 4.0
+s = 'HackerRank '
+
 # Declare second integer, double, and String variables.
 # Read and save an integer, double, and String to your variables.
 i2 = int(input())
-d2 = double(input())
-s2 = String(input())
-
+d2 = float(input())
+s2 = str(input())
 
 # Print the sum of both integer variables on a new line.
 print(i+i2)
@@ -15,4 +18,4 @@ print(d+d2)
 
 # Concatenate and print the String variables on a new line
 # The 's' variable above should be printed first.
-print(Concatenate(s+s2))
+print(s+s2)
