cb0f3632c79ce8892f7b452799209a5f93087a98
pritikadasgupta <pritika.dasgupta@gmail.com>
2018-09-02 13:54:28 -0400

    fizz buzz challenge, 13.6 pts

--- Python3/fizzbuzz.py
+++ Python3/fizzbuzz.py
@@ -1,6 +1,7 @@
-#Score is (200 - # of characters in source code)/100
+#Score is (200 - # of characters in source code)/10
 
 # #Initial solution in Hacker Rank
+# # 2 points
 # i=1
 # while i <= 100:
 #     if i%3==0:
@@ -21,6 +22,12 @@
 
 # # 9.70 points
 # [print("FizzBuzz" if i%15==0 else "Fizz" if i%3==0 else "Buzz" if i%5==0 else i) for i in range(1,101)]
-# 
-# 13.4 points
-[print("Fizz"*(i%3==0)+(i%5==0)*"Buzz"or i) for i in range(1,101)]
+
+# # 13.4 points
+# [print("Fizz"*(i%3==0)+(i%5==0)*"Buzz"or i) for i in range(1,101)]
+
+# 13.6 points
+for(i)in range(1,101):print("Fizz"*(i%3==0)+(i%5==0)*"Buzz"or i)
+
+# # 13.5 points
+# [print("Fizz"*(i%3==0)+(i%5==0)*"Buzz"or i)for(i)in range(1,101)]
