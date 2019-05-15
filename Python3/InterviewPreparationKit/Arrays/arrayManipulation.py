cd3378bf9402dc9c7f6ed12cb3d7a843e875fca3
Unknown <pritika.dasgupta@gmail.com>
2019-05-15 15:57:51 -0400

    comments on arrayManipulation.py

--- Python3/InterviewPreparationKit/Arrays/arrayManipulation.py
+++ Python3/InterviewPreparationKit/Arrays/arrayManipulation.py
@@ -7,6 +7,7 @@ import re
 import sys
 
 # Complete the arrayManipulation function below.
+# Instead of storing the actual values in the array, you store the difference between the current element and the previous element. So you add sum to a[p] showing that a[p] is greater than its previous element by sum. You subtract sum from a[q+1] to show that a[q+1] is less than a[q] by sum (since a[q] was the last element that was added to sum). By the end of all this, you have an array that shows the difference between every successive element. By adding all the positive differences, you get the value of the maximum element
 def arrayManipulation(n, queries):
     arr = [0]*(n+1)
     for i in range(0,len(queries)):
