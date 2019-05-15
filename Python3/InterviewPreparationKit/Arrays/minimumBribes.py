9fd97c6148160e0e64b83e69a1932dd8e3b68bca
Unknown <pritika.dasgupta@gmail.com>
2019-05-15 00:49:06 -0400

    minimumBribes.py - inefficient solution

--- Python3/InterviewPreparationKit/Arrays/minimumBribes.py
+++ Python3/InterviewPreparationKit/Arrays/minimumBribes.py
@@ -7,15 +7,34 @@ import re
 import sys
 
 # Complete the minimumBribes function below.
-def minimumBribes(q):
+def minimumBribes(n,q):
+	# 1 cannot bribe (position 0, position 1)
+	# 2 can only bribe once (position 0 and 1)
+	# other positions can bribe once or twice (position x, x-1, x-2)
+	i=n
+	numBribes=0
+	p=list(range(1,n+1))
+	while (i>-1):
+		if q[i-1]==p[i-1]: 
+			i=i-1
+		elif q[i-2]==p[i-1]:
+			numBribes+=1
+			p[i-1],p[i-2]=p[i-2],p[i-1]
+			i=i-1
+		elif q[i-3]==p[i-1]:
+			numBribes+=2
+			p[i-1],p[i-2]=p[i-2],p[i-1]
+			p[i-2],p[i-3]=p[i-3],p[i-2]
+			i=i-1
+		elif q[i-4]==p[i-1]:
+			numBribes="Too chaotic"
+			i=-1
+	print(numBribes)
+
 
 if __name__ == '__main__':
     t = int(input())
-
     for t_itr in range(t):
         n = int(input())
-
         q = list(map(int, input().rstrip().split()))
-
-        minimumBribes(q)
-        "hi"
+        minimumBribes(n,q)
