f2b14c413b6b08542318df0004c9336011e2ece7
Unknown <pritika.dasgupta@gmail.com>
2019-05-14 21:08:41 -0400

    initial minimumSwaps.py
    working but does not give min num of swaps

---
+++ Python3/InterviewPreparationKit/Arrays/minimumSwaps.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def swap(arr, pos1, pos2): 
+	arr[pos1], arr[pos2] = arr[pos2], arr[pos1] 
+	return arr
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+	# initialize number of swaps
+	numSwaps = 0
+	sortarr = sorted(arr)
+	# find lowest element
+	arrMin = min(arr)
+	#start with 0 position
+	i=0
+	while (arr!=sortarr):
+		pointer = arr.index(arrMin)
+		swap(arr,i,pointer)
+		arrMin+=1
+		i+=1
+		numSwaps+=1
+		print(arr)
+	return numSwaps
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = minimumSwaps(arr)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
