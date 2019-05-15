152d35958a26024d367b1e1ad9434dc0832d03c2
Unknown <pritika.dasgupta@gmail.com>
2019-05-15 01:20:01 -0400

    soln that works?

--- Python3/InterviewPreparationKit/Arrays/minimumBribes.py
+++ Python3/InterviewPreparationKit/Arrays/minimumBribes.py
@@ -11,26 +11,16 @@ def minimumBribes(n,q):
 	# 1 cannot bribe (position 0, position 1)
 	# 2 can only bribe once (position 0 and 1)
 	# other positions can bribe once or twice (position x, x-1, x-2)
-	i=n
+	q=[j-1 for j in q]
 	numBribes=0
-	p=list(range(1,n+1))
-	while (i>-1):
-		if q[i-1]==p[i-1]: 
-			i=i-1
-		elif q[i-2]==p[i-1]:
-			numBribes+=1
-			# p[i-1],p[i-2]=p[i-2],p[i-1]
-			i=i-1
-		elif q[i-3]==p[i-1]:
-			numBribes+=2
-			# p[i-1],p[i-2]=p[i-2],p[i-1]
-			# p[i-2],p[i-3]=p[i-3],p[i-2]
-			i=i-1
-		elif q[i-4]==p[i-1]:
+	p=list(range(0,n))
+	for i,j in enumerate(q):
+		if q[i-4]==p[i-1]:
 			print("Too chaotic")
 			return
-		else:
-			i=i-1
+		for k in range(max(j-1,0),i):
+			if q[k] > j:
+				numBribes+=1
 	print(numBribes)
 
 
