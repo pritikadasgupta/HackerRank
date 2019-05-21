55017c288f5371c3aa1997c250cef2669fd44426
Unknown <pritika.dasgupta@gmail.com>
2019-05-21 00:57:14 -0400

    initialize abbreviation.py

---
+++ Python3/InterviewPreparationKit/Dynamic Programming/abbreviation.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the abbreviation function below.
+def abbreviation(a, b):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        a = input()
+
+        b = input()
+
+        result = abbreviation(a, b)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
